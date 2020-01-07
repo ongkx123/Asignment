5d6ab79ac61425bb44cec1b05b87e27cf0948567
taruc <mikeong356@gmail.com>
2020-01-07 15:54:40 +0800

    hi

--- app/src/main/java/com/example/asignment/Login.kt
+++ app/src/main/java/com/example/asignment/Login.kt
@@ -3,22 +3,39 @@ package com.example.asignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
+    lateinit var ref: DatabaseReference
+    lateinit var userName: String
+    lateinit var password: String
+    private var textUserName: TextInputLayout? = null
+    private var textPassword: TextInputLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        textUserName = findViewById(R.id.textInputUserName)
+        textPassword = findViewById(R.id.textInputPassword)
 
-        buttonRegister.setOnClickListener{
-            val intent = Intent(applicationContext,SignUp_From::class.java)
+        buttonRegister.setOnClickListener {
+            val intent = Intent(applicationContext, SignUp_From::class.java)
             startActivity(intent)
         }
 
-        buttonLogin.setOnClickListener{
-            val intent = Intent(applicationContext,FragmentActivity::class.java)
-            startActivity(intent)
+        buttonLogin.setOnClickListener {
+           login()
         }
+    }
+
+    private fun login(){
+        
     }
 }
