a59bdc0415bc9b29690ebb83d2637bf03b692cc5
CH <leecheehoe0620@gmail.com>
2020-01-07 21:30:30 +0800

    updated

--- app/src/main/java/com/example/asignment/HomeModel.kt
+++ app/src/main/java/com/example/asignment/HomeModel.kt
@@ -1,4 +1,11 @@
 package com.example.asignment
 
-class HomeModel (var id: Int, var likes: Int, var comments: Int, var propic: Int, var postpic: Int, var name: String, var time: String, var status: String)
+class HomeModel (var id: Int,
+                 var likes: Int,
+                 var comments: Int,
+                 var propic: Int,
+                 var postpic: Int,
+                 var name: String,
+                 var time: String,
+                 var status: String)
 
--- app/src/main/java/com/example/asignment/Login.kt
+++ app/src/main/java/com/example/asignment/Login.kt
@@ -3,6 +3,9 @@ package com.example.asignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
@@ -17,8 +20,19 @@ class Login : AppCompatActivity() {
         }
 
         buttonLogin.setOnClickListener{
-            val intent = Intent(applicationContext,FragmentActivity::class.java)
-            startActivity(intent)
+            //val intent = Intent(applicationContext,Menu::class.java)
+            //startActivity(intent)
+
+            login()
+        }
+    }
+
+    private fun login(){
+        var user:User
+        val ref = FirebaseDatabase.getInstance().getReference("User")
+        fun onDataChange(dataSnapshot: DataSnapshot){
+            user = dataSnapshot.getValue() as User
+            //Toast.makeText(applicationContext,user,Toast.LENGTH_SHORT).show()
         }
     }
 }
