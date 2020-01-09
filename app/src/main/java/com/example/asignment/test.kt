4f81dd5bac5d2dd7aaf8c29b5bd843de8950a70a
karsen99 <kscheah1@gmail.com>
2020-01-09 22:40:32 +0800

    Commit

---
+++ app/src/main/java/com/example/asignment/test.kt
@@ -0,0 +1,12 @@
+package com.example.asignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class test : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
