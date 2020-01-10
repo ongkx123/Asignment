9415d9a82e433adf46ed2f41cec2958c070e4fe5
siehchingyong <siehcy-wm17@student.tarc.edu.my>
2020-01-10 22:29:56 +0800

    initial

--- app/src/main/java/com/example/asignment/MFragmentActivity.kt
+++ app/src/main/java/com/example/asignment/MFragmentActivity.kt
@@ -2,11 +2,52 @@ package com.example.asignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MFragmentActivity : AppCompatActivity() {
+
+    lateinit var videoFragment : MenuVideo
+    lateinit var articleFragment : MenuArticle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mfragment)
+
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottomNavigation)
+
+        videoFragment= MenuVideo()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout,videoFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+
+                R.id.video -> {
+                    videoFragment= MenuVideo()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,videoFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.article -> {
+                    articleFragment= MenuArticle()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,articleFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+
+            true
+        }
     }
 }
